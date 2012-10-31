e133c686525fbd4b7da067e96703ef1e16736979
hehopmajieh <gamishev@gmail.com>
2012-10-31 18:17:21 +0200

    adress spinner added and working

--- gen/axon/test/testJNI/R.java
+++ gen/axon/test/testJNI/R.java
@@ -24,6 +24,8 @@ public final class R {
         public static final int button3=0x7f060003;
         public static final int button4=0x7f060004;
         public static final int button5=0x7f060005;
+        public static final int button6=0x7f060010;
+        public static final int editText1=0x7f060011;
         public static final int spinner1=0x7f060000;
         public static final int textView1=0x7f060006;
         public static final int textView10=0x7f06000f;
--- src/axon/test/testJNI/TestJNIActivity.java
+++ src/axon/test/testJNI/TestJNIActivity.java
@@ -2,47 +2,67 @@ package axon.test.testJNI;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class TestJNIActivity extends Activity {
     private static final String TAG = "I2C-Test";
     axonI2C i2c;
-    int slaveAddr = 0x58;
+    int slaveAddr=0x58;
     int relay = 0;
     int[] state = new int[4];
 	int bits;
 	double adc1,adc2,adc3,adc4;
 	TextView DPort1,DPort2,DPort3,DPort4,APort1,APort2,APort3,APort4;
+	EditText AddrInput;
 	private Spinner spinner1;
 	/** Called when the activity is first created. */
-    @Override
+   
+	public void addItemsOnSpinner() {
+		 
+		spinner1 = (Spinner) findViewById(R.id.spinner1);
+		List<String> list = new ArrayList<String>();
+		list.add("0x58");
+		list.add("0x59");
+		list.add("0x5A");
+		list.add("0x5B");
+		list.add("0x5C");
+		list.add("0x5D");
+		list.add("0x5E");
+		list.add("0x5F");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+			android.R.layout.simple_spinner_item, list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner1.setAdapter(dataAdapter);
+	  }
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-    
-     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.addresses, android.R.layout.simple_spinner_item);
-     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-     spinner.setAdapter(adapter);
-       
         
+       
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         i2c = new axonI2C();
         state[0] = 0;
 		state[1] = 0;
 		state[2] = 0;
 		state[3] = 0; //save restore/state	
         //toDo : States @!
-		 
+		addItemsOnSpinner();
 		
 		new Thread() {
         	@Override
@@ -55,6 +75,8 @@ public class TestJNIActivity extends Activity {
 						public void run() {
 						
 							DPort1 = (TextView)findViewById(R.id.textView2); //Analog Port Labels
+							AddrInput = (EditText)findViewById(R.id.editText1);
+							
 							DPort2 = (TextView)findViewById(R.id.textView3);
 							DPort3 = (TextView)findViewById(R.id.textView4);
 							DPort4 = (TextView)findViewById(R.id.textView5);
@@ -64,7 +86,8 @@ public class TestJNIActivity extends Activity {
 							APort3 = (TextView)findViewById(R.id.textView9);
 							APort4 = (TextView)findViewById(R.id.textView10);
 							
-							
+							Log.i(TAG, String.valueOf(spinner1.getSelectedItem()));
+							slaveAddr = Integer.decode(String.valueOf(spinner1.getSelectedItem()));
 							String[] hin1 = { "su", "-c","chmod 777 /dev/i2c-2" };  //gain root access
 						        try {
 						                Runtime.getRuntime().exec(hin1);
@@ -72,15 +95,15 @@ public class TestJNIActivity extends Activity {
 						        // TODO Auto-generated catch block
 						        e.printStackTrace();
 						      }
+						        ;
 						       
-						        
-								final Button button = (Button) findViewById(R.id.button1);
+						        final Button button = (Button) findViewById(R.id.button1);
 					        button.setOnClickListener(new View.OnClickListener() {
 					        	
 					 		 
 					        	public void onClick(View v) {
 					            
-					            
+					        		
 					            	WriteI2C(slaveAddr, 1); //ToDo: check port 
 					           
 					            }
@@ -115,13 +138,25 @@ public class TestJNIActivity extends Activity {
 					           
 					            }
 					        });							       
+					        final Button button6 = (Button) findViewById(R.id.button6);
+					        button6.setOnClickListener(new View.OnClickListener() {
+					            public void onClick(View v) {
+					            
+					            	//Log.i(TAG,)
+					            DPort1.setText(AddrInput.getText().toString());
+					           
+					           
+					            }
+					        });			      
+					        
 					        
 					        switch   (ReadI2CDigital(slaveAddr,0x20)) {
-					        case 1 : DPort1.setText("Digital Port1: High"); break;
+					        case 1 : //DPort1.setText("Digital Port1: High"); 
+					        	break;
 					        case 2 : DPort2.setText("Digital Port2: High"); break;
 					        case 4 : DPort3.setText("Digital Port3: High"); break;
 					        case 8 : DPort4.setText("Digital Port4: High"); break;
-					        default: DPort1.setText("Digital Port1: Low"); 
+					        default: //DPort1.setText("Digital Port1: Low"); 
 					        		 DPort2.setText("Digital Port2: Low");
 					        		 DPort3.setText("Digital Port3: Low");
 					        		 DPort4.setText("Digital Port4: Low"); break;
@@ -172,7 +207,7 @@ public class TestJNIActivity extends Activity {
     		buf[0] = 0x10; //set reg
     		buf[1] = relay;	 //set out
    
-    		i2c.write(fileHandle, 0x58, 0, buf, 2);    	
+    		i2c.write(fileHandle, slaveAddr, 0, buf, 2);    	
     
         i2c.close(fileHandle);
 		
@@ -192,7 +227,7 @@ public class TestJNIActivity extends Activity {
         	Log.w(TAG, "Could not open I2C interface");
         }
     		buf[0] = register;
-    		i2c.write(fileHandle, 0x58, 0, buf, 1);    	
+    		i2c.write(fileHandle, slaveAddr, 0, buf, 1);    	
         
 
     		i2c.read(fileHandle, slaveAddr, buf, 4);
@@ -220,7 +255,7 @@ public class TestJNIActivity extends Activity {
         }
     		buf[0] = register;
     		
-    		i2c.write(fileHandle, 0x58, 0, buf, 1);    	
+    		i2c.write(fileHandle, slaveAddr, 0, buf, 1);    	
         
 
     		i2c.read(fileHandle, slaveAddr, buf, 4);
@@ -234,5 +269,31 @@ public class TestJNIActivity extends Activity {
 			return buf[0];
     }  
 
+    void ChangeSlaveAddr(int CurrentAddr, int NewAddr)
+    {
+        int fileHandle = 0;
+        int[] buf = new int[4];
+        
+        try
+        {
+        	fileHandle = i2c.open("/dev/i2c-2");
+        }
+        catch(Exception e)
+        {
+        	Log.w(TAG, "Could not open I2C interface");
+        }
+    		buf[0] = 0xF0;
+    		buf[1] = NewAddr;
+    		i2c.write(fileHandle, CurrentAddr, 0, buf, 2);    	
+        
 
+    		
+    		/*Log.w(TAG,
+                    "buf0= " + Integer.toHexString(buf[0]) + " buf1= "
+                             + Integer.toHexString(buf[1]) + " buf2= "
+                             + Integer.toHexString(buf[2]) + " buf=3 "
+                             + Integer.toHexString(buf[3])); */
+ 
+    		i2c.close(fileHandle);
+    }  
 }
